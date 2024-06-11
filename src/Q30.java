db3a2c82596ce2e5e44730f525b1d06bf5fbe1fd
Bhavya Jain <jainbhavya2306@gmail.com>
2024-06-12 00:37:59 +0530

    12

--- src/Q30.java
+++ src/Q30.java
@@ -26,51 +26,8 @@
 // The substring starting at 9 is "barthefoo". It is the concatenation of ["bar","the","foo"].
 // The substring starting at 12 is "thefoobar". It is the concatenation of ["the","foo","bar"].
 
-import java.util.List;
-import java.util.ArrayList;
-
 public class Q30 {
     public static List<Integer> findSubstring(String s, String[] words) {
-        List<Integer> result = new ArrayList<>();
-        if (s == null || s.length() == 0 || words == null || words.length == 0) {
-            return result;
-        }
-        int wordLen = words[0].length();
-        int totalLen = wordLen * words.length;
-        if (s.length() < totalLen) {
-            return result;
-        }
-        int[] map = new int[256];
-        for (String word : words) {
-            map[word.charAt(0)]++;
-        }
-        for (int i = 0; i < wordLen; i++) {
-            int left = i;
-            int right = i;
-            int count = 0;
-            while (right + wordLen <= s.length()) {
-                char c = s.charAt(right);
-                right += wordLen;
-                if (map[c]-- > 0) {
-                    count++;
-                }
-                while (map[c] < 0) {
-                    char d = s.charAt(left);
-                    map[d]++;
-                    if (map[d] > 0) {
-                        count--;
-                    }
-                    left += wordLen;
-                }
-                if (count == words.length) {
-                    result.add(left);
-                }
-            }
-            map = new int[256];
-            for (String word : words) {
-                map[word.charAt(0)]++;
-            }
-        }
-        return result;
+        
     }
 }
