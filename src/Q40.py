7c8b5b5d4ef4f7fa12783ad97ef494758886a3f1
Bhavya Jain <jainbhavya2306@gmail.com>
2024-06-30 00:01:20 +0530

    solved

--- src/Q40.py
+++ src/Q40.py
@@ -9,11 +9,28 @@
 # Example 2:
 # Input: candidates = [2,5,2,1,2], target = 5
 # Output: [[1,2,2],[5]]
-def combinationSum2(self, candidates, target):
-    """
-    :type candidates: List[int]
-    :type target: int
-    :rtype: List[List[int]]
-    """
-    if not candidates or len(candidates)==0: return []
+class Solution(object):
+    def helper(self, res, path, candidates, target, index):
+        if target == 0:
+            res.append(list(path))
+            return
+        if target < 0:
+            return
         
+        for i in range(index, len(candidates)):
+            if i > index and candidates[i] == candidates[i - 1]:
+                continue
+            path.append(candidates[i])
+            self.helper(res, path, candidates, target - candidates[i], i + 1)
+            path.pop()
+    
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        candidates.sort()
+        res = []
+        self.helper(res, [], candidates, target, 0)
+        return res
