bf2ebf69c118dabfc934dd40443298eb71aa5bb5
Bhavya Jain <jainbhavya2306@gmail.com>
2024-08-10 13:22:40 +0530

    reverseListRecursive

--- src/Q206.java
+++ src/Q206.java
@@ -20,13 +20,22 @@ import DataStructures.ListNode;
 
 public class Q206 {
     public ListNode reverseList(ListNode head) {
-        ListNode prev=null,curr=head,next;
-        while(curr!=null) {
+        ListNode prev = null, curr = head, next;
+        while (curr != null) {
             next = curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
         return prev;
+    }
+
+    public ListNode reverseListRecursive(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode newHead = reverseListRecursive(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
     }
 }
