904a3ec570440cbb8e4859293582a545a6e30193
Bhavya Jain <jainbhavya2306@gmail.com>
2024-08-12 19:53:28 +0530

    92

--- src/DataStructures/ListNode.java
+++ src/DataStructures/ListNode.java
@@ -6,4 +6,12 @@ public class ListNode {
 	public ListNode() {}
 	public ListNode(int val) {this.val=val;}
 	public ListNode(int val,ListNode next) {this.val=val;this.next=next;}
+	public void printList() {
+		ListNode temp = this;
+		while(temp!=null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+	}
 }
