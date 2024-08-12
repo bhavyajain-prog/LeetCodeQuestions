7b2fc321338a433bdd2cae35af1c4bfe96144220
Bhavya Jain <jainbhavya2306@gmail.com>
2024-08-12 22:20:49 +0530

    119-updated

--- src/Q119.java
+++ src/Q119.java
@@ -37,6 +37,23 @@ public class Q119 {
         return rows.getLast();
     }
 
+    // Another Solution using formula
+    
+    // public List<Integer> getRow(int rowIndex) {
+    // List<Integer> ans = new ArrayList<>(); // create a new arraylist
+    // ans.add(1); // Add the first element of the row, which is always 1
+    // if (rowIndex == 0)
+    // return ans; // If rowIndex is 0, return 1
+    // long coeff = 1; // use a coeff varibale to store value
+
+    // for (int i = 1; i < rowIndex; ++i) {
+    // coeff = (coeff * (rowIndex - (i - 1))) / i;
+    // ans.add((int) coeff);
+    // }
+    // ans.add(1); // Add the last element which is always 1
+    // return ans;
+    // }
+
     public static void main(String[] args) {
         System.out.println(getRow(3));
     }
