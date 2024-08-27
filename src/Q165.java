5be43e87670b78b5850acacfe776d5256455cbfc
Bhavya Jain <jainbhavya2306@gmail.com>
2024-08-27 12:30:21 +0530

    165

--- src/Q165.java
+++ src/Q165.java
@@ -34,8 +34,8 @@ public class Q165 {
     public static int compareVersion(String version1, String version2) {
         String[] v1Parts = version1.split("\\.");
         String[] v2Parts = version2.split("\\.");
-        int maxLength = Math.max(v1Parts.length, v2Parts.length);
-        for (int i = 0; i < maxLength; i++) {
+        int n = Math.max(v1Parts.length, v2Parts.length);
+        for (int i = 0; i < n; i++) {
             int v1 = i < v1Parts.length ? Integer.parseInt(v1Parts[i]) : 0;
             int v2 = i < v2Parts.length ? Integer.parseInt(v2Parts[i]) : 0;
             if (v1 < v2) {
